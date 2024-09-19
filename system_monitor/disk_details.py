50565bd159d929181fe1f04cb8ef42881f758d0e
Isaac <i247175@dac.unicamp.br>
2024-09-19 10:45:32 -0300

    fix: type and string bugs fixed

--- system_monitor/disk_details.py
+++ system_monitor/disk_details.py
@@ -3,15 +3,15 @@
 import psutil
 
 
-usage = psutil.disk_usage('/')
+usage = list(psutil.disk_usage('/'))
 for i in range(3):
-    usage[i] = usage[i] / 10^9 # converts bytes in gb
+    usage[i] = usage[i] / 10**9 # converts bytes in gb
 usage_perc = usage[-1]
 
 def run():
     print("========== DISK DETAILS ==========")
-    print("/ Partition Usage Percentage: " + usage_perc)
-    print("Used space: " + usage_perc[1])
-    print("Free space: " + usage_perc[2])
-    print("Total space: " + usage_perc[0])
-    print("================================\n")
+    print(f"/ Partition Usage: {usage_perc}%")
+    print(f"Used space: {usage[1]:.0f}GB")
+    print(f"Free space: {usage[2]:.0f}GB")
+    print(f"Total space: {usage[0]:.0f}GB")
+    print("==================================\n")
